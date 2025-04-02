62e54db70ee145d0ddd4f887065a16e7aa68c9c9
VictorGHss <vg.hss0101@gmail.com>
2025-04-02 15:14:15 -0300

    Aula sobre estruturas de repetição!!

---
+++ src/dio/introducao/repeticao/ExemploDoWhile.java
@@ -0,0 +1,19 @@
+package dio.introducao.repeticao;
+
+import java.util.Random;
+
+public class ExemploDoWhile {
+    public static void main(String[] args) {
+        System.out.println("Discando...");
+
+        do {
+            System.out.println("Telefone tocando");
+        } while (tocando());
+        System.out.println("\uD83D\uDDE3\uFE0F: Fala corno ");
+    }
+
+    private static boolean tocando() {
+        boolean atendeu = new Random().nextInt(3) == 1;
+        return !atendeu;
+    }
+}
